38a5b52b076b9a2f092ff8bf2c260c575f25c3c3
Richard Littauer <richard.littauer@gmail.com>
2013-12-28 13:05:53 -0500

    Added the sidebar back in. You can toggle it off and on.

--- drought.js
+++ drought.js
@@ -1,12 +1,21 @@
 // Get rid of the sections you don't like
 function clear_page() {
+    var ariaLabels = document.querySelectorAll('[aria-label]');
+
+    for (var i = 0; i < ariaLabels.length; i++) {
+        if (ariaLabels[i].getAttribute("aria-label") == "Hide Sidebar") {
+            ariaLabels[i].click()
+            break;
+        }
+    }
     // I'm personally not a fan of the blue Status bar
     document.getElementsByClassName('_li')[0] = document.getElementsByClassName('_li')[0].removeChild(document.getElementById('pagelet_bluebar'));
     // Or the rest of the page, to be honest
     document.getElementsByClassName('_li')[0] = document.getElementsByClassName('_li')[0].removeChild(document.getElementById('globalContainer'));
-    // The sidebar might be nice, if you want to know which friends are on.
-    // Of course you can detach the chat bar, in the lower right corner. I suggest doing this, as it makes it easier, and you can have it there, but not be distracted by it all of the time. If you don't detach it, you can't see friends - which might be another good option. To re-enable, simply disable the extension, and refresh. 
-    document.getElementsByClassName('_li')[0] = document.getElementsByClassName('_li')[0].removeChild(document.getElementById('pagelet_sidebar'));
+    
+    // If you want to get rid of the chatbar, uncomment the next line. 
+    // document.getElementsByClassName('_li')[0] = document.getElementsByClassName('_li')[0].removeChild(document.getElementById('pagelet_sidebar'));
+    
     // I don't even like the translation globe. Who speaks in other languages?
     // This seems to break it. Needs to be fixed.
     // document.getElementsByClassName('clearfix nubContainer rNubContainer')[0] = document.getElementsByClassName('clearfix nubContainer rNubContainer')[0].removeChild(document.getElementById('fbTranslationsNub'));
@@ -18,7 +27,7 @@ function add_image() {
     var viewportHeight = window.innerHeight;
     var viewportWidth  = window.innerWidth;
     // Edit the src to point to a wallpaper you, you know, like.
-    newcontent.innerHTML = '<div style="background-image:url(http://i.imgur.com/yXABw.jpg);height:' + viewportHeight + 'px;width:' + viewportWidth + 'px;background-size:100%;"></div>';
+    newcontent.innerHTML = '<div style="background-image:url(http://medias.photodeck.com/dda9f1be-1df9-11e1-a648-fdc95b6a7d82/acor0707_xgaplus.jpg);height:' + viewportHeight + 'px;width:' + viewportWidth + 'px;background-size:100%;"></div>';
 
     while (newcontent.firstChild) {
         mydiv[0].appendChild(newcontent.firstChild);
@@ -28,4 +37,3 @@ function add_image() {
 // And we're done here, folks.
 clear_page();
 add_image();
-
